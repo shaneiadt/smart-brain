b54281692f795337083f0a61111d31d8220cf87d
Shane O'Moore <Shane.OMoore@livetiles.nyc>
2019-11-11 09:03:04 +0000

    Setup serverless Lambda function to display emoji rank

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Rank = ({ name, entries }) => {
+  const [emoji, setEmoji] = useState('');
+
+  useEffect(() => {
+    generateEmoji(entries);
+  }, [entries]);
+
+  function generateEmoji(entries) {
+    fetch(`https://xv6vrffpe9.execute-api.us-east-1.amazonaws.com/prod/rank?rank=${entries}`)
+      .then(res => res.json())
+      .then(data => {
+        setEmoji(data.input);
+      }).catch(err => console.error(err));
+  }
+
   return (
     <div>
       <div className='white f3'>
@@ -8,6 +22,9 @@ const Rank = ({ name, entries }) => {
       </div>
       <div className='white f1'>
         {entries}
+      </div>
+      <div className='white f3'>
+        {`Rank Badge: ${emoji}`}
       </div>
     </div>
   );
